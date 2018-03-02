bade2ad75d7335382892e9789eb0bcc1c010dff5
William Entriken <github.com@phor.net>
2018-03-02 11:29:45 -0500

    Fix price to 0.5 ether, fixes #2

--- contracts/SuVending.sol
+++ contracts/SuVending.sol
@@ -10,7 +10,7 @@ import "./SuNFT.sol";
 contract SuVending is SuNFT {
     uint256 public constant SALE_PRICE = 500 finney; // 0.5 ether
 
-    /// @notice The price is always 1 ether, and you can buy any available Square
+    /// @notice The price is always 0.5 ether, and you can buy any available Square
     ///  Be sure you are calling this from a regular account (not a smart contract)
     ///  or if you are calling from a smart contract, make sure it can use
     ///  ERC-721 non-fungible tokens
